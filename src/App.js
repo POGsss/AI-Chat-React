880b6056ebdd1786beed1e6c53b49637725a2dec
POGsss <frankiepinat14@gmail.com>
2025-01-12 23:52:48 +0800

    Design Structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
+import whiteBotLogo from "./assets/whiteBotLogo.png";
+
 function App() {
   return (
     <div className="App">
-      
+      <div className="container">
+        <div className="chatContainer">
+          <div className="chatHeader">
+            <div className="chatHeaderTitle">
+              <img src={whiteBotLogo} alt="" />
+              <h1>Chat Bot</h1>
+            </div>
+            <div className="chatHeaderActions">
+              
+            </div>
+          </div>
+          <div className="chatBody">
+            
+          </div>
+          <div className="chatFooter">
+
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
