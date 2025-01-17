4f61f5facaba979df710ee0b6cf70603ad89986d
POGsss <frankiepinat14@gmail.com>
2025-01-17 16:38:03 +0800

    Added Modal

---
+++ src/components/Modal.js
@@ -0,0 +1,15 @@
+import { HiX } from "react-icons/hi";
+
+function Modal({ open, onClose, title, description }) {
+    return (
+        <div onClick={onClose} className={`backdrop ${open ? "open" : ""}`}>
+            <div onClick={(e) => e.stopPropagation} className={`backdropModal ${open ? "open" : ""}`}>
+                <HiX className="closeBtn"/>
+                <h1>{title}</h1>
+                <p>{description}</p>
+            </div>
+        </div>
+    );
+}
+
+export default Modal;
